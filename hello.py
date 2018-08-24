a65dbc33a9f57b34eb0f49fdc2290d1c49853e03
Niranjan M.R <mrniranjan@redhat.com>
2018-08-24 17:19:08 +0530

    Tests: Add niranjan to the attende list
    Signed-off-by: Niranjan M.R <mrniranjan@redhat.com>
    Reviewed-by: Fabiano Fidêncio <fidencio@redhat.com>

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 print("Welcome to the crash course!")
 
-attendees = ["fidencio", "Madhuri", "Dan", "Steeve", "pratik"]
+attendees = ["fidencio", "Madhuri", "Dan", "Steeve", "pratik", "niranjan"]
 
 for attendee in attendees:
     print("- %s" % (attendee))
